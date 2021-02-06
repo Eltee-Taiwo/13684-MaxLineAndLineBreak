77305a02d96feee76d6677449940b3b26612a1f0
Eltee <lytaiwo@hotmail.com>
2021-02-06 17:33:50 -0600

    Initial Commit.

---
+++ MaxLineAndLineBreak/MaxLineAndLineBreak/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MaxLineAndLineBreak
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new List<string>
+            {
+                "Short Text",
+                "This is some medium text on the page",
+                "This is extremely long text on the page. I don't know why I'm typing this out when I could just copy some dummy text from somewhere."
+            };
+        }
+    }
+}
